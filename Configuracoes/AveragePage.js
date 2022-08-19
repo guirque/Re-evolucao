529993a03e10e0a00ad3d065da9e757df57dded1
Guilherme <111164179+guirque@users.noreply.github.com>
2022-08-18 22:04:01 -0300

    Add files via upload

---
+++ Configuracoes/AveragePage.js
@@ -0,0 +1,119 @@
+//Question Declarations
+var Questions =
+{
+    //q (question) + number
+    q1:
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/a/ab/Dendrobates_pumilio.jpg", //url of demonstrative image
+        text: "O sapo na imagem &eacute; um caso de", //the question itself
+        opt1: "Camuflagem", //option 1
+        opt2: "Aposematismo", //option 2
+        opt3: "Mimetismo", //option 3
+        opt4: "M&uuml;llerismo", //option 4
+        rightOpt: 2 //right option
+    },
+    q2:
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/b/b0/Parc_Asterix_20.jpg", //url of demonstrative image
+        text: "Tubar&otilde;es e golfinhos possuem suas semelhan&ccedil;as devido ao processo de", //the question itself
+        opt1: "Alto Grau de Parentesco", //option 1
+        opt2: "Mimetismo", //option 2
+        opt3: "Homologia", //option 3
+        opt4: "Converg&ecirc;ncia Adaptativa", //option 4
+        rightOpt: 4 //right option
+    },
+    q3:
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/9/97/Darwin%27s_finches.jpeg", //url of demonstrative image
+        text: "Em casos de Irradia&ccedil;&atilde;o Adaptativa, a homologia entre os seres em quest&atilde;o &eacute;...?", //the question itself
+        opt1: "Alta", //option 1
+        opt2: "Baixa", //option 2
+        opt3: "Nula", //option 3
+        opt4: "Igual", //option 4
+        rightOpt: 1 //right option
+    },
+    q4:
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/f/f7/Giraffe%21_%284565230826%29.jpg", //url of demonstrative image
+        text: "De acordo com a teoria de <b>Darwin</b>, o n&uacute;mero crescente de girafas com pesco&ccedil;os maiores &eacute; docorrente do(a)", //the question itself
+        opt1: "Sele&ccedil;&atilde;o Natural, em que as girafas alongam seus pesco&ccedil;os por muito tempo para alcan&ccedil;ar alimentos em &aacute;rvores, fator que contribui para a perman&ecirc;ncia da caracter&iacute;stica ao longo de gera&ccedil;&otilde;es.", //option 1
+        opt2: "Transdu&ccedil;&atilde;o, em que as girafas trocam material gen&eacute;tico por meio de bacteri&oacute;fagos, aumentando suas chances de sobreviver.", //option 2
+        opt3: "Sele&ccedil;&atilde;o Natural, em que as girafas com a caracter&iacute;stica mencionada tendem a ter maiores chances de sobreviv&ecirc;ncia, fator que, com o devido tempo, prevalece a exist&ecirc;ncia somente dos mais aptos.", //option 3
+        opt4: "Adapta&ccedil;&atilde;o ao Meio, em que as girafas com a caracter&iacute;stica mencionada tendem a ter maiores chances de sobreviv&ecirc;ncia, fator que, com o devido tempo, prevalece a exist&ecirc;ncia somente dos mais aptos.", //option 4
+        rightOpt: 3 //right option
+    },
+    q5:
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/c/ca/Giraffe.JPG", //url of demonstrative image
+        text: "De acordo com a teoria de <b>Lamarck</b>, o n&uacute;mero crescente de girafas com pesco&ccedil;os maiores &eacute; docorrente do (a)", //the question itself
+        opt1: "Adapta&ccedil;&atilde;o ao Meio, em que as girafas alongam seus pesco&ccedil;os por muito tempo para alcan&ccedil;ar alimentos em &aacute;rvores, fator que contribui para a mudan&ccedil;a da caracter&iacute;stica ao longo de gera&ccedil;&otilde;es.", //option 1
+        opt2: "Transforma&ccedil;&atilde;o, em que as girafas trocam material gen&eacute;tico com defuntos, adquirindo anticorpos que as protegem do que antes as afetavam. Pesco&ccedil;os menores eram causados por uma doen&ccedil;a chamada de Brevicollo.", //option 2
+        opt3: "Sele&ccedil;&atilde;o Natural, em que as girafas com a caracter&iacute;stica mencionada tendem a ter maiores chances de sobreviv&ecirc;ncia, fator que, com o devido tempo, prevalece a exist&ecirc;ncia somente dos mais aptos.", //option 3
+        opt4: "Adapta&ccedil;&atilde;o ao Meio, em que as girafas com a caracter&iacute;stica mencionada tendem a ter maiores chances de sobreviv&ecirc;ncia, fator que, com o devido tempo, prevalece a exist&ecirc;ncia somente dos mais aptos.", //option 4
+        rightOpt: 1 //right option
+    },
+    q6:
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/2/2e/Sleeping_dogs_lie_%2831001440016%29.jpg", //url of demonstrative image
+        text: "Durante muitos anos, o humano encontrou no lobo um companheiro. J&aacute; nos dias atuais, &eacute comum ver seus descendentes, cachorros, com diversas cores de pelagem, temperamento e altura em locais que nem favorecem tais especificidades. O principal motivo para isso &eacute;  ", //the question itself
+        opt1: "A Sele&ccedil;&atilde;o Natural, em que as ra&ccedil;as mais aptas sobrevivem e passam suas caracter&iacute;sticas adiante.", //option 1
+        opt2: "A Adapta&ccedil;&atilde;o ao meio, em que cachorros vivem em moradias diferentes e, por isso, desenvolvem diferentes apar&ecirc;ncias.", //option 2
+        opt3: "A Sele&ccedil;&atilde;o Artificial, em que o avan&ccedil;o da medicina permitiu com que cachorros realizassem o processo de meiose 5x mais. Assim, puderam herdar o produto de milhares de cruzamentos em que houve crossing-over.", //option 3
+        opt4: "A Sele&ccedil;&atilde;o Artificial, em que humanos cruzam cachorros de diferentes caracter&iacute;sticas com o objetivo de obter certa expressividade fenot&iacute;pica", //option 4
+        rightOpt: 4 //right option
+    },
+    q7:
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/0/0a/Boretti_Arbre_niveau.png", //url of demonstrative image
+        text: "\"Uma hip&oacute;tese evolutiva popular diz que mudan&ccedil;as comportamentais em novos ambientes anulam os efeitos da sele&ccedil;&atilde;o natural. Mas o trabalho de Jonathan Losos e seus colaboradores na Universidade de Harvard, em 2003, empresta pouco apoio a essa teoria. Os pesquisadores introduziram um grande lagarto terr&iacute;cola e predador, Leiocephalus carinatus, a seis ilhas pequenas nas Bahamas, com seis outras ilhas servindo como controle. Descobriram que a presa desse lagarto, que &eacute; um lagarto menor chamado Anolis sagrei, passava mais tempo em maiores alturas na vegeta&ccedil;&atilde;o de ilhas ocupadas pelo predador do que em ilhas onde L. carinatus estava ausente. Mas a mortalidade de A. sagrei continuou mais alta nas ilhas experimentais que nas ilhas controle. A presen&ccedil;a do predador maior selecionou em favor de machos de lagarto A. sagreicom pernas mais altas, que podem correr mais r&aacute;pido, e tamb&eacute;m favoreceu f&ecirc;meas maiores, que s&atilde;o tanto mais r&aacute;pidas quanto mais dif&iacute;ceis de vencer e de ingerir.\" <br><br> O texto extra&iacute;do do site <i>Deuses e Homens</i> explica a rela&ccedil;&atilde;o entre o tamanho dos lagartos de certo <i>habitat</i> e as mudan&ccedil;as por eles apresentadas ap&oacute;s determinado tempo. A teoria que &eacute; confirmada pela an&aacute;lise do animal citado &eacute;:", //the question itself
+        opt1: "Darwinismo", //option 1
+        opt2: "Lamarckismo", //option 2
+        opt3: "Criacionismo", //option 3
+        opt4: "Nenhuma das op&ccedil;&otilde;es acima", //option 4
+        rightOpt: 1 //right option
+    },
+    q8:
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/8/8e/Fly_%2828082468026%29.jpg", //url of demonstrative image
+        text: "A asa do morcego e a asa de um inseto possuem suas semelhan&ccedil;as devido &agrave(s):", //the question itself
+        opt1: "Semelhan&ccedil;as adaptativas geradas pelo conv&iacutevio em ambientes que privilegiam seres voadores. Um caso de homologia.", //option 1
+        opt2: "Origem embrion&aacute;ria comum aos dois seres. Um caso de homologia.", //option 2
+        opt3: "Semelhan&ccedil;as adaptativas geradas pelo conv&iacutevio em ambientes que privilegiam seres voadores. Um caso de analogia.", //option 3
+        opt4: "Adapta&ccedil;&atilde;o ao meio dos dois animais, que s&atilde;o privilegiados pela exist&ecirc;ncia de predadores incapazes de atingi-los. Um caso de homologia.", //option 4
+        rightOpt: 3 //right option
+    },
+    q9:
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Juancito.jpg", //url of demonstrative image
+        text: "A exist&ecirc;ncia de novas esp&eacute;cies, dentre as op&ccedil;&otilde;es, podem originar de: ", //the question itself
+        opt1: "Cruzamentos entre seres de mesma fam&iacute;lia.", //option 1
+        opt2: "Cruzamentos entre seres de esp&eacute;cies diferentes, dando origem a animais h&iacute;bridos e est&eacute;reis, como o Canis lupus familiaris.", //option 2
+        opt3: "Cruzamentos entre seres de esp&eacute;cies diferentes, dando origem a animais h&iacute;bridos e est&eacute;reis, como o Equus africanus (asno).", //option 3
+        opt4: "Cruzamentos entre seres de filos diferentes, dando origem a seres h&iacute;bridos e est&eacute;reis, como o Homo Musca domestica.", //option 4
+        rightOpt: 3 //right option
+    },
+    q10:
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/b/bd/Side_blotched_lizard_%28Genus_Uta%29_%2814232233185%29.jpg", //url of demonstrative image
+        text: "Qual &eacute;, dos seguintes casos, o do animal da imagem?", //the question itself
+        opt1: "Mimetismo", //option 1
+        opt2: "Camuflagem", //option 2
+        opt3: "Aposematismo", //option 3
+        opt4: "Lamarckismo", //option 4
+        rightOpt: 2 //right option
+    }
+};
+
+//launching correction cards
+for (var i = 1; i < 11; i++)
+{
+    document.getElementById("main").innerHTML += '<div class="question-card"> <label id="text' + i + '">Question</label><br /> <img id = "image' + i + '" alt = "question image" width = "400" class="image"> <br /> <label id="opt1' + i + '"><input type="radio" name="option" />Opção 1</label><br /> <label id="opt2' + i + '"><input type="radio" name="option" />Opção 2</label><br /> <label id="opt3' + i + '"><input type="radio" name="option" />Opção 3</label><br /> <label id="opt4' + i + '"><input type="radio" name="option" />Opção 4</label><br /><div id="answer' + i + '" class="Answer" style="display: block;"></div></div>';
+
+    document.getElementById("text" + i).innerHTML = Questions["q" + i].text;
+    document.getElementById("image" + i).src = Questions["q" + i].image;
+    document.getElementById("opt1" + i).innerHTML = '<input type="radio" name="option" class="InputRadio" id="RadioOpt1"> ' + Questions["q" + i].opt1;
+    document.getElementById("opt2" + i).innerHTML = '<input type="radio" name="option" class="InputRadio" id="RadioOpt2"> ' + Questions["q" + i].opt2;
+    document.getElementById("opt3" + i).innerHTML = '<input type="radio" name="option" class="InputRadio" id="RadioOpt3"> ' + Questions["q" + i].opt3;
+    document.getElementById("opt4" + i).innerHTML = '<input type="radio" name="option" class="InputRadio" id="RadioOpt4"> ' + Questions["q" + i].opt4;
+    document.getElementById("answer" + i).innerHTML = 'Gabarito: Op&ccedil;&atilde;o ' + Questions["q" + i].rightOpt;
+}
